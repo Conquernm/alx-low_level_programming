378b63a6fce1edeaee416ba8d0351cc57b61f816
Conquernm <nghwazinm@gmail,com>
2023-06-17 15:13:06 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return : 0 on success
+ */
+int main(void)
+{
+	int n = '0';
+	int j = '0';
+
+	while (n <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(n > j) || n ==j)
+			{
+				putchar(i);
+				putchar(j);
+				if (n == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	                j++;
+	 	}
+                j = '0';
+		n++;
+	 }
+return (0);
+}
