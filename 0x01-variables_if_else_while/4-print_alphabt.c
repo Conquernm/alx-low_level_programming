b1cd446b48f0915afed4c8fe3b808d69eeb5fc5a
Conquernm <nghwazinm@gmail,com>
2023-06-17 10:38:41 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,19 @@
  * Return: Always 0
  */
 int main(void)
-{       
-        int n = 97;
- 
+{
+	int n = 97;
+
 	while (n <= 122)
-        {
-                if (n == 101 || n == 113)
-	        { 
-		        n++;
-		        continue;
-	        }
-	        putchar(n);
+	{
+	if (n == 101 || n == 113)
+	{
+		n++;
+		continue;
+	}
+	putchar(n);
 		n++;
 	}
 	putchar('\n');
 	return (0);
-}       
+}
