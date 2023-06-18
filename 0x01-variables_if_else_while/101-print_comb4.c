f361d8821ad7b4a02db8a34491fe9c6c8f2577bc
Conquernm <nghwazinm@gmail,com>
2023-06-18 16:02:42 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'Function to print combination of three numbers'
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int n = '0';
+	int j = '0';
+	int k = '0';
+
+	while (n <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{		
+				if ((n < j) && (n < k) && (j < k) && ((j != n) != k))
+				{
+					putchar(n);
+					putchar(j);
+					putchar(k);
+				if (n == '7' && j == '8' && k == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+				
+			}
+			j++;
+		}
+		k = '0';
+		J = '0';
+		n++;
+	}
+			
+return (0);
+}
