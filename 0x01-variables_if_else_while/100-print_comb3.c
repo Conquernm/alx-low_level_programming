257e329455bf397c579ccc7985b5d1d8d0ab61f2
Conquernm <nghwazinm@gmail,com>
2023-06-18 12:08:10 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 	int n = '0';
 	int j = '0';
 
-	while (n <= '9')
+	while (n <= '8')
 	{
 		while (j <= '9')
 		{
@@ -30,7 +30,7 @@ int main(void)
 			}
 			j++;
 		}
-		j = '0';
+		j = '1';
 		n++;
 	}
 return (0);
