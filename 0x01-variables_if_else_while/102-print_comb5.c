62b85354d1e5a185b9a349391095c64da7865940
Conquernm <nghwazinm@gmail,com>
2023-06-18 19:32:53 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: 'Function to print combination of two numbers'
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; i <= 99; i++)
+	{
+		for (m = 0; m <= 99 : m++)
+		{
+			if ((n < m && i != j)
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (n != 98 || m != 99)
+				}
+					putchar(',')
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
