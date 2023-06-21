c84c905ec0d69f5031031b6777694c2b309fbc07
Conquernm <nghwazinm@gmail,com>
2023-06-20 20:11:46 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n; The number to start printing from
+ * @n: The number to start printing from
  * Return: Always 0.
  */
 
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 {
 
-	if ( n <= 98)
+	if (n <= 98)
 	{
 	for (; n <= 98; n++)
 	{
