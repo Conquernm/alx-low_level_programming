43cec5fb33a504d68a898c2a19e7c939df70d653
Conquernm <nghwazinm@gmail,com>
2023-06-20 17:17:08 -0700

    102-print_comb5.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- *_islower - Checks for loercase characters
- *@C: The character to be checked
- *Return: 1 if character is lowercase or 0 for anything else
+ *_islower - Checks for lowercase characters
+ *c: The character to be checked
+ *Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
+
 {
 
-	if (c >= '97' && c <= '122')
+	if (c >= 97 && c <= 122)
 	{
 	return (1);
 	}	
