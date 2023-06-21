e96b1064d76e1c86377641df697fd1ca56dbeedf
Conquernm <nghwazinm@gmail,com>
2023-06-21 12:10:41 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, staring with 1 and 2, seperated by a coma followed by a space.
+ * main - Prints the first 98 Fibonacci numbers, staring with 1 and 2.
  * Return: Always 0.
  */
 
@@ -24,7 +24,7 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-	for (count =93; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
