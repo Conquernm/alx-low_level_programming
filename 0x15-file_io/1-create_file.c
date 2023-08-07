3f0222b1590b4f7427be050719d07dc39ebf44fd
Conquernm <nghwazinm@gmail,com>
2023-08-07 11:03:23 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
