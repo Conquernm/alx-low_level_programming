65ee3551fc6069f0a36ac0d355e4374dbbcca2ea
Conquernm <nghwazinm@gmail,com>
2023-08-07 12:06:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int file, result = 1, countm = 0;
+int file, result = 1, countn = 0;
 
 if (!filename)
 return (-1);
@@ -18,9 +18,9 @@ if (file == -1)
 return (-1);
 if (text_content)
 
-while (text_content[countm])
-countm++;
-result = write(file, text_content, countm);
+while (text_content[countn])
+countn++;
+result = write(file, text_content, countn);
 
 if (result == -1)
 return (-1);
