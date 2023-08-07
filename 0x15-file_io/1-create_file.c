dda053baf30e55a7eef8c6c7e690291a982228e6
Conquernm <nghwazinm@gmail,com>
2023-08-07 12:19:53 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function that creates a file
  * @filename: name of the file to create
- * @text_content: NULL-terminated string to write to the file
+ * @text_content: string to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -25,5 +25,5 @@ result = write(file, text_content, countn);
 if (result == -1)
 return (-1);
 close(file);
-return (-1);
+return (1);
 }
